1e8d52ea771f085b9987fa1f8a6e44282c3c31f4
李海涛 <15393243613@163.com>
2017-06-01 18:21:23 +0800

    后台布局开发完成

---
+++ js/modules/index.js
@@ -0,0 +1,37 @@
+layui.config({
+    base: '/js/modules/'
+}).use(['element', 'layer'], function() {
+    var element = layui.element(),
+        $layui_elem_quote = $('.layui-elem-quote'),
+        $layui_body_container = $('.layui-body-container');
+    //监听导航事件,获取导航数据
+    element.on('nav', function(element) {
+        var $element = $(element),
+            navtitle = $element.text(),
+            url = $element.children().data().url;
+        if (navtitle !== "") {
+            $layui_elem_quote.text(navtitle);
+            ajaxhtml(url);
+        } else {
+            return;
+        }
+    });
+
+    function ajaxhtml(url) {
+        if (url == "" || undefined) return;
+        //显示加载loading动画
+        layer.load(2);
+        //请求成功，关闭loading动画
+        $layui_body_container.load(url, function(response, status, xhr) {
+            if (status === "success") {
+                layer.closeAll('loading');
+            } else if (status === "error") {
+                layer.closeAll('loading');
+                layer.msg("加载失败，请检查网络链接是否正常！");
+            }
+        });
+    }
+
+
+
+});
