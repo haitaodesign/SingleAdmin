52f5ce6aa422510f3300a889edae2b6e47b3750c
李海涛 <15393243613@163.com>
2017-08-28 15:09:18 +0800

    档案详情改为页面跳转，抽离页面加载方法为模块

--- js/modules/index.js
+++ js/modules/index.js
@@ -2,9 +2,10 @@ layui.config({
     version:false, 
     debug:false,
     base:'/js/modules/'
-}).use(['element','layer'], function(){
+}).use(['element','layer','ajaxhtml'], function(){
     var element = layui.element,
         $ = layui.$;
+    var ajaxhtml = layui.ajaxhtml;
     //应该将一些公共的模块抽离出去，目前现考虑实现功能
     element.on('nav(nav)',function(element){
         var $element = $(element),
@@ -12,7 +13,8 @@ layui.config({
         //添加面包屑导航
         addBreadcrumb($element);
 
-        ajaxhtml(addurl);
+        ajaxhtml.pageLoad(addurl);
+        // ajaxhtml(addurl);
         
     });
 
@@ -46,26 +48,8 @@ layui.config({
         var $layui_nav_one = $('.layui-nav-tree').children()[0],
             one_url = $($layui_nav_one).children().data().url;
         if (one_url !== "") {
-                ajaxhtml(one_url);
+                // ajaxhtml(one_url);
+            ajaxhtml.pageLoad(one_url);
         }
     })();
-
-    function ajaxhtml(url) {
-        $layui_body_container = $('.layui-body-container')
-        $layui_body_container.html("");
-        if (url === "" || undefined) return;
-        //显示加载loading动画
-        layer.load(2);
-        //请求成功，关闭loading动画
-        $layui_body_container.load(url, function(response, status, xhr) {
-            if (status === "success") {
-                layer.closeAll('loading');
-            } else if (status === "error") {
-                layer.closeAll('loading');
-                layer.msg("加载失败，请检查网络链接是否正常！");
-            }
-        });
-    }
-            
-    
 });
